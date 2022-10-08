66d8a9ce06721b6e0862fb1ce4259a017a98d4d7
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-10-08 20:08:50 -0300

    chore: added color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,6 +14,19 @@ module.exports = {
       fontFamily: {
         'sans': ['Mulish', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'primary': '#174666',
+        'gray': {
+          '100': '#F4F5F7',
+          '200': '#DEE2E6',
+          '300': '#8D8D8D',
+          '400': '#737F8B',
+          '600': '#484848',
+          '700': '#404040',
+          '800': '#1F1F1F',
+        },
+        'aux': '#F5D111'
+      }
     },
   },
   plugins: [],
