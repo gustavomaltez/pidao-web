86c61de45cc070dd3ef86ef07fd8d761fc4db9c9
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-10-08 21:30:09 -0300

    feat: added login screen

---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,42 @@
+import { Button, Input } from '@components';
+
+import logo from './images/logo.png';
+
+export function LoginScreen(): JSX.Element {
+  return (
+    <main className={getMainContainerClassName()}>
+      <img src={logo} className="w-16 mx-auto my-4" />
+      <form className="flex flex-col gap-4 mx-auto w-full">
+        <Input
+          id='email'
+          type='email'
+          label='E-mail'
+          placeholder='Insira seu e-mail'
+        />
+        <Input
+          id='password'
+          type='password'
+          label='Senha'
+          placeholder='Insira sua senha'
+        />
+        <Button
+          theme='primary'
+          label='Entrar'
+          type='submit'
+        />
+      </form>
+    </main>
+  );
+}
+
+// Helpers ---------------------------------------------------------------------
+
+function getMainContainerClassName(): string {
+  const classes = [
+    'flex flex-col items-center justify-center',
+    'bg-white rounded-lg',
+    'absolute -translate-y-2/4 -translate-x-2/4 left-2/4 top-2/4',
+    'max-w-xl w-[90vw] p-4'
+  ];
+  return classes.join(' ');
+}
