495b3204dd6bd6e92e3e74883eae0446916e9be7
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-10-23 23:18:42 -0300

    feat: added toast container to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { ToastContainer } from 'react-toastify';
 
 import { AppRouter } from './router/AppRouter';
 
 export function App(): JSX.Element {
-  return <AppRouter />;
+  return (
+    <>
+      <AppRouter />
+      <ToastContainer />
+    </>
+  );
 }
