85e0abcf0924235f95e900520cd718fbaf25cbf8
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-10-24 11:13:55 -0300

    feat: updated fakeapi

--- src/fakeapi/index.ts
+++ src/fakeapi/index.ts
@@ -10,8 +10,8 @@ export function startFakeAPI() {
     },
 
     seeds(server) {
-      server.create('user', { email: 'admin@gmail.com', password: 'admin', type: 'admin' } as any);
-      server.create('user', { email: 'client@gmail.com', password: 'client', type: 'client' } as any);
+      server.create('user', { email: 'admin@gmail.com', password: 'admin', type: 'admin', name: 'Zézinho' } as any);
+      server.create('user', { email: 'client@gmail.com', password: 'client', type: 'client', name: 'Gustavo Maltez' } as any);
       const pizzaImageURL = 'https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80';
       const hamburgerImageURL = 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80';
       const sodaImageURL = 'https://images.unsplash.com/photo-1585498154575-3db0fda49f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
@@ -25,6 +25,70 @@ export function startFakeAPI() {
       server.create('item', { name: 'Coca-Cola', price: 9.99, quantity: 3, category: 'Bebidas', image: sodaImageURL } as any);
       server.create('item', { name: 'Guaraná', price: 9.99, quantity: 20, category: 'Bebidas', image: sodaImageURL } as any);
       server.create('item', { name: 'Fanta', price: 9.99, quantity: 13, category: 'Bebidas', image: sodaImageURL } as any);
+      server.create('order', {
+        'items': [
+          {
+            'name': 'Pizza Brotinho',
+            'price': 9.99,
+            'quantity': 1,
+            'category': 'Pizza',
+            'image': 'https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80',
+            'id': '1',
+            'totalAvailable': 3
+          },
+          {
+            'name': 'Pizza Média',
+            'price': 19.99,
+            'quantity': 1,
+            'category': 'Pizza',
+            'image': 'https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=428&q=80',
+            'id': '2',
+            'totalAvailable': 7
+          },
+          {
+            'name': 'Hamburger Duplo',
+            'price': 19.99,
+            'quantity': 1,
+            'category': 'Hamburger',
+            'image': 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80',
+            'id': '6',
+            'totalAvailable': 4
+          },
+          {
+            'name': 'Hamburger Triplo',
+            'price': 29.99,
+            'quantity': 1,
+            'category': 'Hamburger',
+            'image': 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=999&q=80',
+            'id': '7',
+            'totalAvailable': 7
+          },
+          {
+            'name': 'Fanta',
+            'price': 9.99,
+            'quantity': 1,
+            'category': 'Bebidas',
+            'image': 'https://images.unsplash.com/photo-1585498154575-3db0fda49f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+            'id': '10',
+            'totalAvailable': 13
+          },
+          {
+            'name': 'Coca-Cola',
+            'price': 9.99,
+            'quantity': 1,
+            'category': 'Bebidas',
+            'image': 'https://images.unsplash.com/photo-1585498154575-3db0fda49f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+            'id': '8',
+            'totalAvailable': 3
+          }
+        ],
+        'client': {
+          'email': 'client@gmail.com',
+          'type': 'client',
+          'id': '2',
+          'name': 'Gustavo Maltez'
+        }
+      } as any);
     },
 
     routes() {
@@ -38,15 +102,15 @@ export function startFakeAPI() {
       });
 
       this.post('/api/register', (schema: any, request) => {
-        const { email, password } = JSON.parse(request.requestBody);
-        if (schema.users.findBy({ email })) {
+        const data = JSON.parse(request.requestBody);
+        if (schema.users.findBy({ email: data.email })) {
           return {
             status: 400,
             error: 'Já existe um usuário com esse e-mail',
           };
         }
 
-        const user = schema.users.create({ email, password });
+        const user = schema.users.create(data);
         const userWithoutPassword = { ...user.attrs };
         delete userWithoutPassword.password;
 
@@ -87,6 +151,10 @@ export function startFakeAPI() {
           itemFromDB.update({ quantity: itemFromDB.quantity - item.quantity });
         });
 
+        return schema.orders.all();
+      });
+
+      this.get('/api/orders', (schema: any) => {
         return schema.orders.all();
       });
     },
